a85a8c87531739579af8558c7480544f54720ee0
ellienori <ellienorihere@gmail.com>
2022-02-06 13:48:01 +0900

    coin tracker

--- src/App.js
+++ src/App.js
@@ -1,38 +1,32 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState(""); // defalt value: empty string
-  const [toDos, setToDos] = useState([]); // defalt value: empty array
-  const onChange = (event) => {
-    setToDo(event.target.value);
-  };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === "") {
-      return ;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
+  const [loading, setLoading] = useState(true); // defalt value: true
+  const [coins, setCoins] = useState([]); // default value: empty array
 
-  console.log(toDos);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=200")
+      .then((res) => res.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []); // []가 비어 있으면 (아무것도 주시하고 있지 않으면) 이 함수는 한 번만 실행 된다.
 
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-          value={toDo} 
-          onChange={onChange} 
-          text="text" 
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => <li key={index}>{item}</li>)}
-      </ul>
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? (
+        <strong>Loading.....</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
