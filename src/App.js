e99c7a03ec16d8dc98ece647b480aed07770bc06
ellienori <ellienorihere@gmail.com>
2022-02-06 10:56:11 +0900

    useEffect(effect-callback, dependency-list)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import Button from "./Button";
-import styles from "./App.module.css"
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => setValue((current) => current+1);
+  //console.log("I run all the time.");
+  useEffect(() => console.log("I run only once."), []);
+
+  const onChange = (event) => setKeyword(event.target.value);
+  
+  useEffect(() => { // keyword가 바뀔 때만 검색하고 싶어. 지금은 버튼 누를 때도 검색이 됨
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]); // keyword가 변화할 때 코드를 실행할거라고 react.js에게 알려주는 거야
+
+  useEffect(() => console.log("I run when 'counter' changes."), [counter]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text="Continue🤍"/>
+      <input onChange={onChange} type="text" placeholder="Search here...." />
+      <h1>{counter}!</h1>
+      <button onClick={onClick}>click me 🤍</button>
     </div>
   );
 }
