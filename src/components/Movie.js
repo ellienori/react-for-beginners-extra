013a1fb5849905c3be02ec5f543c6dcb5489805c
ellienori <ellienorihere@gmail.com>
2022-02-06 15:46:37 +0900

    movie summary length

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,13 +2,19 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 function Movie({ coverImg, title, id, summary, genres }) {
+  const STRING_LEN = 235;
+
   return (
     <div>
       <img src={coverImg} alt={title}></img>
       <h2>
         <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
-      <p>{summary}</p>
+      <p>
+        {summary.length > STRING_LEN
+          ? `${summary.slice(0, STRING_LEN)}...`
+          : summary}
+      </p>
       <ul>
         {genres.map((genre) => (
           <li key={genre}>{genre}</li>
